00d5be3976e97d8b62d2d0f2217344d15d3446a8
Tishaya <118938566+Kiyo-io@users.noreply.github.com>
2024-01-22 02:36:58 -0500

    cleaned up code to properly function

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
     var buttonInnerHTML = this.innerHTML;
 
-playSound(buttonInnerHTML)
-
+    playSound(buttonInnerHTML);
   });
 }
 
-
 document.addEventListener("keydown", function (event) {
-
   playSound(event.key);
-
 });
 
-function playSound(key){
-
+function playSound(key) {
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -65,9 +59,4 @@ function playSound(key){
     default:
       console.log(buttonInnerHTML);
   }
-});
-}
-
-
-
 }
