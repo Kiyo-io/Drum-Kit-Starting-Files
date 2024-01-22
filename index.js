f15eff4202cb018f3bbb4481a263ca2b5430ff49
Tishaya <118938566+Kiyo-io@users.noreply.github.com>
2024-01-22 01:24:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+
+        break;
+
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+
+        break;
+      default:
+        break;
+    }
+  });
+}
